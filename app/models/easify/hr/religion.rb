fa5e7a2bbc733cc3dcec8cbfc912cbb6fb74ae05
Wilson Moreno <wilson_moreno@1stslp.com>
2016-08-12 17:53:17 +0800

    Additional rspec testing

--- app/models/easify/hr/religion.rb
+++ app/models/easify/hr/religion.rb
@@ -1,2 +1,3 @@
 class Easify::Hr::Religion < ActiveRecord::Base
+      has_many :members, class_name: "Easify::Hr::HumanResource", foreign_key: "religion_id"
 end
--- spec/models/easify/hr/religion_spec.rb
+++ spec/models/easify/hr/religion_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Easify::Hr::Religion, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+   it "has many member Human Resources" do
+      association = Easify::Hr::Religion.reflect_on_association(:members)
+      expect(association.macro).to be(:has_many)
+   end
+
 end
