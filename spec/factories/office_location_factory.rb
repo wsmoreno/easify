cc4702a91298117034a261ee2fc53a09382dcb01
Wilson Moreno <wilson_moreno@1stslp.com>
2016-09-05 17:49:50 +0800

    validations

--- spec/factories/office_location_factory.rb
+++ spec/factories/office_location_factory.rb
@@ -5,6 +5,7 @@ FactoryGirl.define do
       address1 "20th Floor Marco Polo"
       address2 "Ortigas Manila, Sapphire Road, Ortigas Center"
       phone_number "916-1111"
+      start_date 1.year.ago
   end
 
 end
