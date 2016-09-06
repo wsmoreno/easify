662f92e442854ca567be38d192db04c033bda0a5
Wilson Moreno <wilson_moreno@1stslp.com>
2016-09-06 09:43:28 +0800

    Testing

--- app/models/easify/hr/relative.rb
+++ app/models/easify/hr/relative.rb
@@ -1,5 +1,17 @@
 class Easify::Hr::Relative < ActiveRecord::Base
-  belongs_to :human_resource, class_name: "Easify::Hr::HumanResource", foreign_key: "human_resource_id"
-  belongs_to :relationship, class_name: "Easify::Hr::Relationship", foreign_key: "relationship_id"
-  belongs_to :city, class_name: "Easify::Hr::City", foreign_key: "city_id"
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :middle_name, presence: true
+  validates :birthdate, presence: true
+  validates :relationship, presence: true  
+  validates :gender, presence: true
+  validates :address1, presence: true
+  validates :city, presence: true
+  validates :email_address, format: { with: VALID_EMAIL_REGEX }
+
+  belongs_to :human_resource, class_name: "Easify::Hr::HumanResource", foreign_key: "easify_hr_human_resource_id"
+  belongs_to :relationship, class_name: "Easify::Hr::Relationship", foreign_key: "easify_hr_relationship_id"
+  belongs_to :city, class_name: "Easify::Hr::City", foreign_key: "easify_hr_city_id"
 end
--- spec/models/easify/hr/relative_spec.rb
+++ spec/models/easify/hr/relative_spec.rb
@@ -2,6 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Easify::Hr::Relative, type: :model do
 
+  before do
+    relative = build(:relative)
+    relationship = build(:relationship)
+    city = build(:city)
+    relative.relationship = relationship
+    relative.city = city
+    relative.save
+  end
+
   it "belongs to a Human Resource" do
      association = Easify::Hr::Relative.reflect_on_association(:human_resource)
      expect(association.macro).to be(:belongs_to)
@@ -17,23 +26,110 @@ RSpec.describe Easify::Hr::Relative, type: :model do
      expect(association.macro).to be(:belongs_to)
   end
 
-  it "should not allow blank first name"
-  it "should not allow blank last name"
-  it "should not allow blank middle name"
-  it "should not allow blank birth date"
-  it "should not allow blank relationship"
-  it "should not allow blank gender"
-  it "should not allow blank address 1"
-  it "should not allow blank city" 
-  it "should not allow invalid email address"
-  it "should be valid with non-blank first name"
-  it "should be valid with non-blank last name"
-  it "should be valid with non-blank middle name" 
-  it "should be valid with non-blank birth date"
-  it "should be valid with non-blank relationship"
-  it "should be valid with non-blank gender"
-  it "should be valid with non-blank address 1"
-  it "should be valid with non-blank city"
+  it "should not allow blank first name" do
+     relative = build(:relative, first_name: nil)
+     relative.valid?
+     expect(relative.errors[:first_name]).to include("can't be blank")
+  end
+
+  it "should not allow blank last name" do
+     relative = build(:relative, last_name: nil)
+     relative.valid?
+     expect(relative.errors[:last_name]).to include("can't be blank")
+  end
+
+  it "should not allow blank middle name" do
+     relative = build(:relative, middle_name: nil)
+     relative.valid?
+     expect(relative.errors[:middle_name]).to include("can't be blank")
+  end
+
+  it "should not allow blank birth date" do
+     relative = build(:relative, birthdate: nil)
+     relative.valid?
+     expect(relative.errors[:birthdate]).to include("can't be blank")
+  end
+ 
+  it "should not allow blank relationship" do
+     relative = build(:relative, relationship: nil)
+     relative.valid?
+     expect(relative.errors[:relationship]).to include("can't be blank")
+  end
+
+  it "should not allow blank gender" do
+     relative = build(:relative, gender: nil)
+     relative.valid?
+     expect(relative.errors[:gender]).to include("can't be blank")
+  end
+
+  it "should not allow blank address 1" do
+     relative = build(:relative, address1: nil)
+     relative.valid?
+     expect(relative.errors[:address1]).to include("can't be blank")
+  end
+
+
+  it "should not allow blank city" do
+     relative = build(:relative, city: nil)
+     relative.valid?
+     expect(relative.errors[:city]).to include("can't be blank")
+  end
+ 
+  it "should not allow invalid email address" do
+    relative = build(:relative, email_address: "asdfqweg;asdf")
+    relative.valid?
+    expect(relative.errors[:email_address]).to include("is invalid")
+  end
+  
+  it "should be valid with non-blank first name" do
+    relative = build(:relative)
+    relative.valid?
+    expect(relative.errors[:first_name]).not_to include("can't be blank")
+  end
+
+  it "should be valid with non-blank last name" do
+     relative = build(:relative)
+     relative.valid?
+     expect(relative.errors[:last_name]).not_to include("can't be blank")
+  end
+
+  it "should be valid with non-blank middle name" do
+     relative = build(:relative)
+     relative.valid?
+     expect(relative.errors[:middle_name]).not_to include("can't be blank")
+  end
+  
+  it "should be valid with non-blank birth date" do
+     relative = build(:relative)
+     relative.valid?
+     expect(relative.errors[:birthdate]).not_to include("can't be blank")
+  end
+
+  it "should be valid with non-blank relationship" do
+     relative = build(:relative)
+     relative.relationship = build(:relationship)
+     relative.valid?
+     expect(relative.errors[:relationship]).not_to include("can't be blank")
+  end
+
+  it "should be valid with non-blank gender" do
+     relative = build(:relative)
+     relative.valid?
+     expect(relative.errors[:gender]).not_to include("can't be blank")
+  end
+
+  it "should be valid with non-blank address 1" do
+     relative = build(:relative)
+     relative.valid?
+     expect(relative.errors[:address1]).not_to include("can't be blank")
+  end
+
+  it "should be valid with non-blank city" do
+     relative = build(:relative)
+     relative.city = build(:city)
+     relative.valid?
+     expect(relative.errors[:city]).not_to include("can't be blank")
+  end
 
 end
 
