7495cda653aa97c7f9ab3bd3e65e097ab7af6226
Wilson Moreno <wilson_moreno@1stslp.com>
2016-09-07 09:10:03 +0800

    Testing models

--- spec/models/easify/hr/work_experience_spec.rb
+++ spec/models/easify/hr/work_experience_spec.rb
@@ -18,31 +18,31 @@ RSpec.describe Easify::Hr::WorkExperience, type: :model do
    end
 
    it "should not allow blank employer name" do
-      experience = build(:work_experience)
+      experience = build(:work_experience, employer_name: nil)
       experience.valid?
       expect(experience.errors[:employer_name]).to include("can't be blank")
    end
 
    it "should not allow blank address1" do
-      experience = build(:work_experience)
+      experience = build(:work_experience, address1: nil)
       experience.valid?
       expect(experience.errors[:address1]).to include("can't be blank")
    end
 
    it "should not allow blank start period" do
-      experience = build(:work_experience)
+      experience = build(:work_experience, start_period: nil)
       experience.valid?
       expect(experience.errors[:start_period]).to include("can't be blank")
    end
 
    it "should not allow blank human resource" do
-      experience = build(:work_experience)
+      experience = build(:work_experience, human_resource: nil)
       experience.valid?
       expect(experience.errors[:human_resource]).to include("can't be blank")
    end
 
    it "should not allow blank city" do
-      experience = build(:work_experience)
+      experience = build(:work_experience, city: nil)
       experience.valid?
       expect(experience.errors[:city]).to include("can't be blank")
    end
@@ -53,9 +53,29 @@ RSpec.describe Easify::Hr::WorkExperience, type: :model do
       expect(experience.errors[:employer_name]).not_to include("can't be blank")
    end
 
-   it "should be valid with non-blank address1"
-   it "should be valid with non-blank start period"
-   it "should be valid with non-blank human resource"
-   it "should be valid with non-blank city"
+   it "should be valid with non-blank address1" do
+      experience = build(:work_experience)
+      experience.valid?
+      expect(experience.errors[:address1]).not_to include("can't be blank")
+   end
+
+   it "should be valid with non-blank start period" do
+      experience = build(:work_experience)
+      experience.valid?
+      expect(experience.errors[:start_period]).not_to include("can't be blank")
+   end
+
+   it "should be valid with non-blank human resource" do
+      experience = build(:work_experience)
+      experience.valid?
+      expect(experience.errors[:human_resourece]).not_to include("can't be blank")
+   end
+
+   it "should be valid with non-blank city" do
+      experience = build(:work_experience)
+      experience.city = build(:city)
+      experience.valid?
+      expect(experience.errors[:city]).not_to include("can't be blank")
+   end
 
 end
