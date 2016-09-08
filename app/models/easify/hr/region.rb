a80750dc87bc6fbea7e14ddc1693761835d2a5f1
Wilson Moreno <wilson_moreno@1stslp.com>
2016-09-08 17:59:41 +0800

    Testing

--- app/models/easify/hr/region.rb
+++ app/models/easify/hr/region.rb
@@ -1,2 +1,8 @@
 class Easify::Hr::Region < ActiveRecord::Base
+    validates :name, presence: true
+    validates :island_group, presence: true
+    validates :start_date, presence: true
+
+    belongs_to :country, class_name: "Easify::Hr::Country", foreign_key: "easify_hr_country_id"
+    has_many   :provinces, class_name: "Easify::Hr::Province", foreign_key: "easify_hr_region_id"
 end
--- spec/models/easify/hr/region_spec.rb
+++ spec/models/easify/hr/region_spec.rb
@@ -1,16 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe Easify::Hr::Region, type: :model do
-  
-    it "should belong to a country"
-    it "should have many provinces"
+   
+    before do
+       region = build(:region)
+       region.country = build(:country)
+       region.save
+    end
+    
+    it "should belong to a country" do
+       association = Easify::Hr::Region.reflect_on_association(:country)
+       expect(association.macro).to be(:belongs_to)
+    end
+
+    it "should have many provinces" do
+       association = Easify::Hr::Region.reflect_on_association(:provinces)
+       expect(association.macro).to be(:has_many)
+    end
+
     it "should return a country of type Easify::Hr::Country"
     it "should return provinces of type Easify::Province"
-    it "should not allow blank name"
-    it "should not allow blank island group"
-    it "should not allow blank start date"
-    it "should be valid with non-blank name"
-    it "should be valid with non-blank island group"
-    it "should be valid with non-blank start date"  
+  
+    it "should not allow blank name" do
+       region = build(:region, name: nil)
+       region.valid?
+       expect(region.errors[:name]).to include("can't be blank")
+    end
+
+
+    it "should not allow blank island group" do
+       region = build(:region, island_group: nil)
+       region.valid?
+       expect(region.errors[:island_group]).to include("can't be blank")
+    end 
+
+    it "should not allow blank start date" do
+       region = build(:region, start_date: nil)
+       region.valid?
+       expect(region.errors[:start_date]).to include("can't be blank")
+    end
+
+    it "should be valid with non-blank name" do
+       region = build(:region)
+       region.valid?
+       expect(region.errors[:name]).not_to include("can't be blank")
+    end
+
+    it "should be valid with non-blank island group" do
+       region = build(:region)
+       region.valid?
+       expect(region.errors[:island_group]).not_to include("can't be blank")
+    end
+
+    it "should be valid with non-blank start date" do
+       region = build(:region)
+       region.valid?
+       expect(region.errors[:start_date]).not_to include("can't be blank")
+    end 
 
 end
